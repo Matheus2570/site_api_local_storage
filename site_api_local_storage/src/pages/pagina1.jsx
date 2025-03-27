c83762cbdf11fd7428818c740bb1262fbf0420fd
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-03-27 08:13:12 -0300

    Dia 27/03/2025

--- site_api_local_storage/src/pages/pagina1.jsx
+++ site_api_local_storage/src/pages/pagina1.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
- 
+import "./pagina1.css"
+
 function Pagina1() {
   const [paises, setPais] = useState(null);
- 
+
   useEffect(() => {
     const paisSalvo = localStorage.getItem("paisSelecionado");
     if (paisSalvo) {
-     
-        setPais(JSON.parse(paisSalvo));
-      }
+      setPais(JSON.parse(paisSalvo));
+    }
   }, []);
- 
 
- 
+  if (!paises) {
+    return <div>Carregando...</div>;
+  }
+
   return (
-<div>
-<h2>Detalhes do País</h2>
-<p><strong>Nome Oficial:</strong> {paises.nomeOficial}</p>
-<p><strong>Capital:</strong> {paises.capital}</p>
-<p><strong>Língua:</strong> {paises.lingua}</p>
-
-
-</div>
+    <div>
+      <div className="container">
+        <div className="card">
+        <h2 className="titulo">Detalhes do País</h2>
+      <p><strong>Nome Oficial:</strong> {paises.nomeOficial}</p>
+      <p><strong>Capital:</strong> {paises.capital}</p>
+      <p><strong>Língua:</strong> {paises.lingua}</p>
+        </div>
+      </div>
+    </div>
   );
 }
- 
+
 export default Pagina1;
